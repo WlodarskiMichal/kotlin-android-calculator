2eeedc0572293c890b6ad0bba9ec29b134ff41a7
howmehow <howmehow@gmail.com>
2020-09-28 22:44:09 +0200

    Trimming zeroes that are trailing at the end, tidy up MathOperations, add basic testing.

--- app/src/main/java/com/howmehow/android/calculator/MathematicalOperations.kt
+++ app/src/main/java/com/howmehow/android/calculator/MathematicalOperations.kt
@@ -11,30 +11,28 @@ open class MathematicalOperations {
     fun addition(number1: Double, number2: Double): String {
         val number = (number1 + number2)
         return if (number.rem(1).equals(0.0)) {
-            number.toLong().toString()
+            longNumberStringReturned(number)
         } else {
-            val decimal = BigDecimal(number).setScale(10, RoundingMode.HALF_UP)
-            decimal.toString()
+            bigDecimalStringReturned(number)
         }
     }
 
     fun subtraction(number1: Double, number2: Double): String {
         val number = (number1 - number2)
         return if (number.rem(1).equals(0.0)) {
-            number.toLong().toString()
+            longNumberStringReturned(number)
         } else {
-            val decimal = BigDecimal(number).setScale(10, RoundingMode.HALF_UP)
-            decimal.toString()
+            bigDecimalStringReturned(number)
         }
     }
+
 
     fun multiplication(number1: Double, number2: Double): String {
         val number = (number1 * number2)
         return if (number.rem(1).equals(0.0)) {
-            number.toLong().toString()
+            longNumberStringReturned(number)
         } else {
-            val decimal = BigDecimal(number).setScale(10, RoundingMode.HALF_UP)
-            decimal.toString()
+            bigDecimalStringReturned(number)
         }
     }
 
@@ -42,10 +40,9 @@ open class MathematicalOperations {
         return if (number1 != 0.0 && number2 != 0.0) {
             val number = (number1 / number2)
             if (number.rem(1).equals(0.0)) {
-                number.toLong().toString()
+                longNumberStringReturned(number)
             } else {
-                val decimal = BigDecimal(number).setScale(10, RoundingMode.HALF_UP)
-                decimal.toString()
+                bigDecimalStringReturned(number)
             }
         } else {
             infinity
@@ -53,6 +50,15 @@ open class MathematicalOperations {
     }
 
     fun squareRoot(number1: Double): String {
-        return sqrt(number1).toString()
+        return sqrt(number1).toString().trimEnd('0')
+    }
+
+    private fun bigDecimalStringReturned(number: Double): String {
+        val decimal = BigDecimal(number).setScale(10, RoundingMode.HALF_UP)
+        return decimal.toString().trimEnd('0')
+    }
+
+    private fun longNumberStringReturned(number: Double): String {
+        return number.toLong().toString().trimEnd('0')
     }
 }
---
+++ app/src/test/java/com/howmehow/android/calculator/PresenterTest.kt
@@ -0,0 +1,233 @@
+package com.howmehow.android.calculator
+
+import org.junit.After
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PresenterTest {
+
+    @After
+    fun tearDown() {
+        Mockito.verifyNoMoreInteractions(view)
+    }
+
+    @Mock
+    lateinit var view: Contract.View
+
+    @Test
+    fun `test plus button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).operationButtonPressed("+")
+
+        //then
+        verify(view).updateTextView("+")
+    }
+
+    @Test
+    fun `test minus button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).operationButtonPressed("-")
+
+        //then
+        verify(view).updateTextView("-")
+    }
+
+    @Test
+    fun `test multiply button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).operationButtonPressed("×")
+
+        //then
+        verify(view).updateTextView("×")
+    }
+
+    @Test
+    fun `test divide button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).operationButtonPressed("÷")
+
+        //then
+        verify(view).updateTextView("÷")
+    }
+
+    @Test
+    fun `test number button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("1")
+
+        //then
+        verify(view).updateTextView("1")
+    }
+
+    @Test
+    fun `test number two button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("2")
+
+        //then
+        verify(view).updateTextView("2")
+    }
+
+    @Test
+    fun `test number three button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("3")
+
+        //then
+        verify(view).updateTextView("3")
+    }
+
+    @Test
+    fun `test number four button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("4")
+
+        //then
+        verify(view).updateTextView("4")
+    }
+
+    @Test
+    fun `test number five button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("5")
+
+        //then
+        verify(view).updateTextView("5")
+    }
+
+    @Test
+    fun `test number six button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("6")
+
+        //then
+        verify(view).updateTextView("6")
+    }
+
+    @Test
+    fun `test number seven button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("7")
+
+        //then
+        verify(view).updateTextView("7")
+    }
+
+    @Test
+    fun `test number eight button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("8")
+
+        //then
+        verify(view).updateTextView("8")
+    }
+
+    @Test
+    fun `test number nine button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("9")
+
+        //then
+        verify(view).updateTextView("9")
+    }
+
+    @Test
+    fun `test number zero button pressed and the user input is shown on text view`() {
+
+        //given - setting the rules/variables
+        val presenter: Contract.Presenter = Presenter()
+        presenter.init(view)
+
+
+        //when - what happens when we do the test
+        (presenter as Presenter).numberButtonPressed("0")
+
+        //then
+        verify(view).updateTextView("0")
+    }
+}
